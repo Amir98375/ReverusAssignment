9a8abcf2b34cbe51b0adee7e84680b3a7fb74a98
amir <mohd.a@test.com>
2024-03-04 13:02:17 +0530

    todo route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-
-import { useState } from 'react';
-import CameraComponent from './Camera';
-import Todo from './Todo';
+import { useState } from "react";
+import CameraComponent from "./Camera";
+import Todo from "./Todo";
 
 function App() {
-  const [cameraFlag,setCameraFlag]=useState(false)
+  const [cameraFlag, setCameraFlag] = useState(false);
+  const [imageUri, setImageUri] = useState(null);
   return (
     <div className="App">
-      <button onClick={()=>setCameraFlag(!cameraFlag)}>Take pic</button>
-      {cameraFlag?<CameraComponent/>:''}
+      {/* <div
+        style={{
+          height: "200px",
+          width: "200px",
+          borderRadius: "100px",
+          border: "2px solid black",
+          alignSelf: "center",
+          margin: "auto",
+        }}
+      ></div>
+      <div
+        style={{
+          margin: "auto",
+          display:'flex',
+          justifyContent:'center'
+        }}
+      >
+        <button onClick={() => setCameraFlag(!cameraFlag)}>Take pic</button>
+      </div>
+      {cameraFlag ? <CameraComponent /> : ""} */}
 
-{/* <Todo/> */}
+      <Todo/>
     </div>
   );
 }
