2320fca10b7e479393ad2cd35934063ab96f906d
amir <mohd.a@test.com>
2024-03-04 10:46:38 +0530

    camera component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import { useState } from 'react';
+import CameraComponent from './Camera';
 import Todo from './Todo';
 
 function App() {
+  const [cameraFlag,setCameraFlag]=useState(false)
   return (
     <div className="App">
+      <button onClick={()=>setCameraFlag(!cameraFlag)}>Take pic</button>
+      {cameraFlag?<CameraComponent/>:''}
 
-<Todo/>
+{/* <Todo/> */}
     </div>
   );
 }
